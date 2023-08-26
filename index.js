962fd5b8c702963045240b01da70150d9fce2dc8
nancy77zion <nancy77zion@gmail.com>
2023-08-26 21:02:33 +0100

    first solution

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let menuRespon = document.getElementById("menu");
+
+  function menuBar() {
+    if (menuRespon.className === "hide-menu") {
+      menuRespon.className += "responsive";
+    } else {
+      menuRespon.className = "hide-menu";
+    }
+  };
+
+  menuRespon.addEventListener('click', menuBar);*/
